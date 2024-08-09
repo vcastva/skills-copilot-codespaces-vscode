ca9951c3dc09a2473fcc24bf2e13744427293e81
vcastva <vale.cvargas@gmail.com>
2024-08-09 03:01:53 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create web server
+// Create a file comments.js
+// Create a web server that listens on port 3000
+// When you visit http://localhost:3000/comments, it should return a list of comments
+// The comments should be stored in a file called comments.json
+// The comments should be read from the file and returned to the user
+// If the file doesn't exist, it should return an empty array
+// If the file is empty, it should return an empty array
+// If the file contains invalid JSON, it should return an error
